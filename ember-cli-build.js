30c71deab51777be179c841206c646cf30de769c
Mickael Palma <mpalma@largus.fr>
2017-08-18 09:06:34 +0200

    Exclude flags

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    fingerprint: {
+      exclude: ['assets/flags']
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
