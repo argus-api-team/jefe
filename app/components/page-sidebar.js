d4e8912f7604db2f03c289905a9a5733e6b3678e
Mickael Palma <mickael.palma@mac.com>
2017-08-20 10:23:55 +0200

    Fix sidebar

--- app/components/page-sidebar.js
+++ app/components/page-sidebar.js
@@ -4,6 +4,9 @@ export default Ember.Component.extend({
   classNames: ['page-sidebar-wrapper'],
 
   didRender() {
+    this.$('.active').closest('.nav-item').removeClass('active')
+    this.$('.active').closest('.top-level').removeClass('active');
+
     this.$('.active').closest('.nav-item').addClass('active');
     this.$('.active').closest('.top-level').addClass('active');
   }
