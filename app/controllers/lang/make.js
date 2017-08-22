67767b71f95d61bbd887fb31d7f3554219ad68f7
Mickael Palma <mpalma@largus.fr>
2017-08-22 09:13:19 +0200

    Sort submodels

---
+++ app/controllers/lang/make.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  sortedSubmodels: Ember.computed('model.submodels', function() {
+    return this.get('model.submodels').sortBy('name')
+  })
+});
