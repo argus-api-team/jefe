ef44a680f26aaa0a60d9ec246c9a350a9b2bcc7b
Mickael Palma <mpalma@largus.fr>
2017-08-22 09:08:35 +0200

    Fix equipment path

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,5 +5,13 @@ export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:oauth2',
   host: 'https://api.largus.fr',
   namespace: 'specs/2.0',
-  coalesceFindRequests: true
+  coalesceFindRequests: true,
+
+  pathForType: function(modelName) {
+    if (modelName === 'equipment') {
+      return 'equipments';
+    } else {
+      return Ember.String.pluralize(modelName);
+    }
+  }
 });
