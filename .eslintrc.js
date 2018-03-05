2777b30b52212318329be0a894a9049e8b39841d
Anthony <anthony@iMac-de-Admin.local>
2018-03-05 17:02:13 +0100

    [Update] Ember and dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,41 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  plugins: [
+    'ember'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended'
+  ],  
   env: {
     browser: true
   },
   rules: {
   },
-  "globals": {
+  globals: {
     "moment": true,
     'App': true,
     'Layout': true,
     'QuickSidebar': true
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        'testem.js',
+        'ember-cli-build.js',
+        'config/**/*.js',
+        'lib/*/index.js'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      }
+    }
+  ]
 };
--- config/environment.js
+++ config/environment.js
@@ -50,9 +50,9 @@ module.exports = function(environment) {
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    ENV.APP.LOG_TRANSITIONS = true;
-    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
     ENV.APP.isSecureCookie = false;
   }
 
@@ -65,6 +65,7 @@ module.exports = function(environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.autoboot = false;
     ENV.APP.isSecureCookie = false;
   }
 
