2fbb228da37dfaec88b872fd77bbb81294339154
Anthony Mangano <amangano@largus.fr>
2018-03-28 12:30:22 +0200

    [Header] Metronic header customization

--- app/components/page-header.js
+++ app/components/page-header.js
@@ -1,12 +1,17 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 import mLayout from 'metronic-layout';
 
 export default Component.extend({
+  i18n: service(),
   session: service('session'),
 
   tagName: 'header',
   classNames: ['m-grid__item', ' m-header'],
+
+  locale: computed.alias('i18n.locale'),
+  locales: computed.alias('i18n.locales'),
 
   didRender() {
     mLayout.initHeader();
