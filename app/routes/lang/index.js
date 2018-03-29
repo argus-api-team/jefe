5495807e30307f981e656f842a2c6e4a216069a1
Anthony Mangano <amangano@largus.fr>
2018-03-29 12:25:44 +0200

    [DashBoard] Get dashboard model

--- app/routes/lang/index.js
+++ app/routes/lang/index.js
@@ -1,4 +1,32 @@
 import EmberRoute from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default EmberRoute.extend({
+  model() {
+    return RSVP.hash({
+      topMakes: this.get('store').query('make', {
+        sort: 'position-quote',
+        page: { size: 10 },
+      }),
+      topVehicles: this.get('store').query('version', {
+        sort: '-quote-ratio',
+        include: 'make,submodel',
+        page: { size: 10 },
+        // filter: {
+        //   quotable: true,
+        // },
+        meta: {
+          filterable:false,
+        }
+      }),
+      latestVehicles: this.get('store').query('version', {
+        sort: '-id',
+        include: 'make,submodel',
+        page: { size: 10 },
+        meta: {
+          filterable: false,
+        }
+      }),
+    });
+  }
 });
