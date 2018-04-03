8b86359c733408aee3ff34fd4af076987876eedb
Anthony Mangano <amangano@largus.fr>
2018-04-03 18:24:30 +0200

    [Make Sheet] Display and filter make models

---
+++ app/controllers/lang/referential/make.js
@@ -0,0 +1,31 @@
+import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import { filter } from 'rsvp';
+import DS from 'ember-data';
+
+export default Controller.extend({
+  filteredCategories: computed('model.make.categories.@each.showCategory', function () {
+    const categories = this.get('model.make.categories');
+    return categories.filterBy('showCategory');
+  }),
+  filteredModels: computed('filteredCategories', 'model.make.model.category', function () {
+    const makeModels = this.get('model.make.models');
+    const filteredCategories = this.get('filteredCategories');
+
+    return DS.PromiseArray.create({
+      promise: filter(makeModels.toArray(), (makeModel) => {
+        const promise = makeModel.get('categories').then((modelCategories) => {
+          let modelIsDisplayed = false;
+          modelCategories.forEach((modelCategory) => {
+            const modelCategoryName = modelCategory.get('name');
+            if (filteredCategories.isAny('name', modelCategoryName)) {
+              modelIsDisplayed = true;
+            }
+          });
+          return modelIsDisplayed;
+        });
+        return promise;
+      }),
+    });
+  }),
+});
--- app/routes/lang/referential/make.js
+++ app/routes/lang/referential/make.js
@@ -3,17 +3,16 @@ import RSVP from 'rsvp';
 
 export default Route.extend({
   model(params) {
-    console.log(params);
     return RSVP.hash({
       make: this.get('store').findRecord('make', params.id, {
-        include: 'models,models.submodels,categories',
+        include: 'models.categories,models.submodels,categories',
         reload: true,
       }),
       topMakeVehicles: this.get('store').query('version', {
         sort: '-quote-ratio',
         include: 'make,submodel',
         page: { size: 5 },
-        filter: {           //Need API update
+        filter: { // Need API update
           // quotable: true,
           // make: params.id
         },
