26cbaaf6af9f24e6478174393951b9bbad0dea48
Anthony Mangano <amangano@largus.fr>
2018-04-10 18:13:04 +0200

    [Vehicle sheet] Integrate optional equipments

--- app/routes/lang/referential/vehicle.js
+++ app/routes/lang/referential/vehicle.js
@@ -6,7 +6,7 @@ export default Route.extend({
   model(params) {
     return RSVP.hash({
       vehicle: this.store.findRecord('version', params.id, {
-        include: 'make,generation,phase,submodel,periods,periods.dimension,periods.tyre,periods.weight,periods.boot,energy,periods.engine,gearbox,periods.transmission,periods.platform,periods.performance,periods.consumption,periods.standard-equipments.feature-category',
+        include: 'make,generation,phase,submodel,periods,periods.dimension,periods.tyre,periods.weight,periods.boot,energy,periods.engine,gearbox,periods.transmission,periods.platform,periods.performance,periods.consumption,periods.standard-equipments.feature-category,periods.optional-equipments.feature-category',
         reload: true,
       }),
       featureCategories: this.store.findAll('featureCategory'),
