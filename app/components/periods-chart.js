9c52ee4ff2e854a01c6ff5b3af9405270b705e64
Anthony Mangano <amangano@largus.fr>
2018-04-13 17:34:47 +0200

    [Vehicule sheet] Integrate prico evolution chart, and ake it interractive

--- app/components/periods-chart.js
+++ app/components/periods-chart.js
@@ -1,17 +1,92 @@
 import Component from '@ember/component';
+import { computed, observer } from '@ember/object';
 import c3 from 'c3';
 
 export default Component.extend({
   didInsertElement() {
+    this.initChart();
+  },
+
+  initChart() {
+    const component = this;
     const componentId = `#${this.get('elementId')}`;
-    c3.generate({
+    const chart = c3.generate({
       bindto: componentId,
       data: {
+        x: 'x',
         columns: [
-          ['data1', 30, 200, 100, 400, 150, 250],
-          ['data2', 50, 20, 10, 40, 15, 25],
+          this.get('periodsStartDates'),
+          this.get('periodsPrices'),
         ],
+        onclick(d) {
+          component.updateSelectedPeriod(d);
+        },
+        selection: {
+          enabled: true,
+          multiple: false,
+        },
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%Y-%m-%d',
+          },
+        },
+      },
+      grid: {
+        x: {
+          show: true,
+        },
+        y: {
+          show: true,
+        },
+      },
+      zoom: {
+        enabled: false,
       },
     });
+    this.set('chart', chart);
+    this.updateSelectedPoint();
   },
+
+  periodsPrices: computed('periods', function () {
+    const periods = this.get('periods');
+    const periodsPrices = periods.mapBy('priceIncludingVat');
+    periodsPrices.unshift('Price TTC');
+    return periodsPrices;
+  }),
+
+  periodsStartDates: computed('periods', function () {
+    const periods = this.get('periods');
+    const periodsStartDates = periods.mapBy('startDate');
+    periodsStartDates.unshift('x');
+    return periodsStartDates;
+  }),
+
+  selectedPeriodObserver: observer('selectedPeriod', function () {
+    this.updateSelectedPoint();
+  }),
+
+  updateSelectedPoint() {
+    const periods = this.get('periods');
+    const selectedPeriod = this.get('selectedPeriod');
+    const chart = this.get('chart');
+    const indexOfSelectedPeriod = periods.indexOf(selectedPeriod);
+    chart.select(['Price TTC'], [indexOfSelectedPeriod], true);
+  },
+
+  updateSelectedPeriod(selectedData) {
+    const periods = this.get('periods');
+    const selectedPeriod = periods.objectAt(selectedData.index);
+    const selectedPeriodId = selectedPeriod.get('id');
+    this.set('selectedPeriod', selectedPeriod);
+    this.set('selectedPeriodId', selectedPeriodId);
+  },
+
+  willDestroy() {
+    const chart = this.get('chart');
+    chart.destroy();
+  },
+
 });
