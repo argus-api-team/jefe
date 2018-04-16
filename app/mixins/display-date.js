1f940b6c0157c3c3ae1fdf7d8bab6b8918b69cd6
Anthony Mangano <amangano@largus.fr>
2018-04-16 11:52:49 +0200

    [Cleanup] Remove metronic global, and use ES6 import for moment

--- app/mixins/display-date.js
+++ app/mixins/display-date.js
@@ -2,14 +2,22 @@ import Mixin from '@ember/object/mixin';
 import { computed } from '@ember/object';
 import { isEmpty } from '@ember/utils';
 import { inject as service } from '@ember/service';
+import moment from 'moment';
 
 export default Mixin.create({
-  moment: service(),
   i18n: service(),
 
   displayDate: computed('startDate', 'endDate', 'i18n.locale', function () {
     const format = 'MMMM YYYY';
+    return this.getDisplayDate(format);
+  }),
 
+  displayDateCompact: computed('startDate', 'endDate', 'i18n.locale', function () {
+    const format = 'MMM YYYY';
+    return this.getDisplayDate(format);
+  }),
+
+  getDisplayDate(format) {
     moment.locale(this.get('i18n.locale'));
     const formatedStartDate = moment(this.get('startDate')).format(format);
     if (isEmpty(this.get('endDate'))) {
@@ -22,5 +30,5 @@ export default Mixin.create({
       startDate: formatedStartDate.toLowerCase(),
       endDate: formatedEndDate.toLowerCase(),
     });
-  }),
+  },
 });
