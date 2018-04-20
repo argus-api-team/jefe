5bd394f8325140a5db0642b5a0fa7bca29972f03
Anthony Mangano <amangano@largus.fr>
2018-04-20 11:50:51 +0200

    [License plate] Display candidates version of a license plate search

---
+++ app/components/candidate-item.js
@@ -0,0 +1,18 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+  store: service(),
+  tagName: 'li',
+  classNames: ['vertical-list__item'],
+  candidate: null,
+  didReceiveAttrs() {
+    const candidateVersionId = this.get('candidate.versionId');
+    const store = this.get('store');
+    store.findRecord('version', candidateVersionId, {
+      include: 'make,submodel',
+    }).then((versionRecord) => {
+      this.set('candidateVersion', versionRecord);
+    });
+  },
+});
