9565aaf3f181a16a1a1eea956ab2a94bb657f4c5
Anthony Mangano <amangano@largus.fr>
2018-04-26 17:36:45 +0200

    [License Pate] Make template responsive

--- app/routes/lang/license-plate.js
+++ app/routes/lang/license-plate.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return this.store.query('matching', {
-      page: { size: 500 },
+      page: { size: 10 },
       sort: '-id',
     });
   },
