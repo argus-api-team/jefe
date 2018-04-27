c4f66df1a615c02f402f312e37de41051bf0ddf3
Anthony Mangano <amangano@largus.fr>
2018-04-27 10:03:06 +0200

    [EsLint] Fix errors

--- app/validators/registration-number.js
+++ app/validators/registration-number.js
@@ -1,11 +1,10 @@
 import BaseValidator from 'ember-cp-validations/validators/base';
-import { translationMacro as t } from "ember-i18n";
 
 const licenseNumberFormat = /^[a-zA-Z]{2}\d{3}[a-zA-Z]{2}$/;
 const oldLicenseNumberFormat = /^\d{1,4}[a-zA-Z]{1,3}\d{2,3}$/;
 
 const RegistrationNumber = BaseValidator.extend({
-  validate(value , options, model /* , attribute */) {
+  validate(value, options, model /* , attribute */) {
     const i18n = model.get('i18n');
     const licensePlateNumber = value.replace(/-/g, '').replace(/\s/g, '');
     const isCurrentLicenseNumberFormat = licenseNumberFormat.test(licensePlateNumber);
