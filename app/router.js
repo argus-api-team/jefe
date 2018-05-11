a21739eaaf84b1d13a2de58e17099a878f71add5
Anthony Mangano <amangano@largus.fr>
2018-05-11 16:11:03 +0200

    [Valorization] Prepare routes for valorization form

--- app/router.js
+++ app/router.js
@@ -9,15 +9,24 @@ const Router = EmberRouter.extend({
 Router.map( function () { // eslint-disable-line
   this.route('lang', { path: '/:lang' }, function () {
     this.route('login');
-    this.route('quote');
+
     this.route('referential', function () {
       this.route('makes', { path: '/' });
       this.route('make', { path: 'make/:id' });
       this.route('model', { path: 'model/:id' });
       this.route('vehicle', { path: 'vehicle/:id' });
     });
+
     this.route('license-plate', function () {
       this.route('matching', { path: '/:id' });
+    });
+
+    this.route('quote', function () {
+      this.route('valorize', { path: '/' }, function () {
+        this.route('vehicle-identification', { path: '/' });
+        this.route('offer');
+        this.route('criteria');
+      });
     });
   });
 });
