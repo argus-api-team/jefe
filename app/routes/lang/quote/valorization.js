f639b163794414c684fe1c324e63f3c6a6b3d8f0
Anthony Mangano <amangano@largus.fr>
2018-06-25 18:17:12 +0200

    [Valorization] Display al value of valorization

--- app/routes/lang/quote/valorization.js
+++ app/routes/lang/quote/valorization.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model(params) {
+    console.log(params)
+    return this.store.findRecord('valorization', params.id, {
+      include: 'values',
+      reload: true,
+    });
+  },
 });
