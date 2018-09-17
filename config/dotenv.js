ccb609bb5c5480367185bb16e758a995c0675326
Mickael Palma <mpalma@largus.fr>
2018-09-17 14:21:39 +0200

    Trying changing dotenv.js configuration

--- config/dotenv.js
+++ config/dotenv.js
@@ -3,6 +3,6 @@ module.exports = function(env) {
     clientAllowedKeys: ['API_URL', 'OAUTH_URL', 'ABLY_KEY'],
     // Fail build when there is missing any of clientAllowedKeys environment variables.
     // By default false.
-    failOnMissingKey: true,
+    failOnMissingKey: false,
   };
 };
