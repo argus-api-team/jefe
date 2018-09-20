9d128ef0532e23811f874652355794f51b95e508
Anthony Mangano <amangano@largus.fr>
2018-09-20 14:19:39 +0200

    [Optimization] Network optimisation for matching and valorization view

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from 'jefe/config/environment';
 import HasManyQuery from 'ember-data-has-many-query';
+import { pluralize } from 'ember-inflector';
 // import Ember from 'ember';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, HasManyQuery.RESTAdapterMixin, {
@@ -9,4 +10,8 @@ export default DS.JSONAPIAdapter.extend(DataAdapterMixin, HasManyQuery.RESTAdapt
   host: ENV.API_URL,
   namespace: 'specs/2.0',
   coalesceFindRequests: true,
+  urlForFindMany(ids, modelName) {
+    const endpoint = pluralize(modelName);
+    return `${this.buildURL()}/${endpoint}/?page[size]=${ids.length}`;
+  },
 });
