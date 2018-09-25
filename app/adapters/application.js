595fdfe4786cdcbfa8cd2ea8463276994bff49da
Anthony Mangano <amangano@largus.fr>
2018-09-25 16:01:30 +0200

    [Depreciation] Remove deprecation from ember simple auth

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,15 +3,21 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from 'jefe/config/environment';
 import HasManyQuery from 'ember-data-has-many-query';
 import { pluralize } from 'ember-inflector';
+import { isPresent } from '@ember/utils';
 // import Ember from 'ember';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, HasManyQuery.RESTAdapterMixin, {
-  authorizer: 'authorizer:oauth2',
   host: ENV.API_URL,
   namespace: 'specs/2.0',
   coalesceFindRequests: true,
   urlForFindMany(ids, modelName) {
     const endpoint = pluralize(modelName);
     return `${this.buildURL()}/${endpoint}/?page[size]=${ids.length}`;
+  },
+  authorize(xhr) {
+    const { accessToken } = this.get('session.data.authenticated');
+    if (isPresent(accessToken)) {
+      xhr.setRequestHeader('Authorization', `Bearer ${accessToken}`);
+    }
   },
 });
