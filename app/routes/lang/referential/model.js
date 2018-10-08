6edaa99d3ac7b47a8561bad5e3ada98810ad87b2
Anthony <amangano@largus.fr>
2018-10-08 20:35:55 +0200

    [WIP] Display table of version for a submodel

--- app/routes/lang/referential/model.js
+++ app/routes/lang/referential/model.js
@@ -1,10 +1,25 @@
 import Route from '@ember/routing/route';
+import { computed } from '@ember/object';
 
 export default Route.extend({
   model(params) {
     return this.store.findRecord('submodel', params.id, {
-      include: 'make,generations.phases.versions',
+      include: this.get('includedRelationship'),
       reload: true,
     });
   },
+  includedRelationship: computed('', function () { // eslint-disable-line
+    const includedRelationship = [
+      'make',
+      // 'generations.phases.versions',
+      // 'periods',
+      // 'periods.engine',
+      'versions.energy',
+      'versions.gearbox',
+      'versions.generation',
+      'versions.phase',
+      // 'periods.transmission',
+    ].join(',');
+    return includedRelationship;
+  }),
 });
