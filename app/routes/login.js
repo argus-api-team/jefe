8312bb34ea2411fa26e1cdda98d73147e62f7962
Anthony <amangano@largus.fr>
2019-02-21 09:25:17 +0100

    [BugFix] Fix settings sync between tab

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,15 +1,6 @@
 import Route from '@ember/routing/route';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
-import { inject as service } from '@ember/service';
 
 export default Route.extend(UnauthenticatedRouteMixin, {
   routeIfAlreadyAuthenticated: 'index',
-
-  userSettings: service(),
-
-  beforeModel() {
-    const userSettings = this.get('userSettings');
-    userSettings.destroy();
-  },
-
 });
