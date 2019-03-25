b0a8933d91bb639ed4227de3cca2a5e13b54b606
Anthony Mangano <amangano@largus.fr>
2019-03-25 18:01:58 +0100

    [EsLint] Try to fix travis job

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,7 +35,12 @@ module.exports = {
     // node files
     {
       files: [
+        '.template-lintrc.js',
+        'ember-cli-build.js',
         'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'lib/*/index.js'
       ],
       parserOptions: {
         sourceType: 'script',
