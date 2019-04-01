26f3671deac7f77a98b5072b1b9fc5ad9dc58985
Anthony Mangano <amangano@largus.fr>
2019-04-01 17:35:43 +0200

    [Alert] install ember notify addon

--- app/routes/not-found.js
+++ app/routes/not-found.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  notify: service('notify'),
   beforeModel() {
     this.transitionTo('index');
+    this.get('notify').error('Page does not exist');
   },
 });
