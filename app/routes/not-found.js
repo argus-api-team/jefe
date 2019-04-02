fdfc39c8df9dcfee8716eb4c910a15579ae8700f
Anthony Mangano <amangano@largus.fr>
2019-04-02 08:50:30 +0200

    [Notify | wip]Customization, display message object values

--- app/routes/not-found.js
+++ app/routes/not-found.js
@@ -5,6 +5,8 @@ export default Route.extend({
   notify: service('notify'),
   beforeModel() {
     this.transitionTo('index');
-    this.get('notify').error('Page does not exist');
+    this.get('notify').error('Page does not exist', {
+      closeAfter: 3600000, // or set to null to disable auto-hiding
+    });
   },
 });
